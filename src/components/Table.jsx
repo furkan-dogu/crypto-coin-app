c853166cc4c9c413287bf6f03d17cfd4c2227c53
Furkan Doğu <furkandogu2018@gmail.com>
2024-03-08 20:30:55 +0300

    coinler doma basıldı

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { FaChartLine } from "react-icons/fa6";
+import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 
 const tableHeader = ["Name", "Rank", "Symbol", "Icon", "Change", "Coin Detail"];
 
-const Table = () => {
+const Table = ({ coins }) => {
+
+    console.log(coins);
+
   return (
     <div className="container d-flex flex-column">
       <table className="table">
@@ -14,15 +19,31 @@ const Table = () => {
           </tr>
         </thead>
 
-        <tbody className="tbody">
-            <tr>
-                <td>Name</td>
-                <td>Name</td>
-                <td>Name</td>
-                <td>Name</td>
-                <td>Name</td>
-                <td>Name</td>
-            </tr>
+        <tbody>
+          {coins.map(
+            ({ name, symbol, iconUrl, change, coinrankingUrl, color }, index) => (
+              <tr key={index}>
+                <td className="fw-bold" style={{color: "gray"}}>{name}</td>
+                <td className="fw-bold" style={{color: `${color}`}}>{index + 1}</td>
+                <td className="fw-bold" style={{color: `${color}`}}>{symbol}</td>
+                <td>
+                    <img src={iconUrl} alt={name} width={30} height={30} />
+                </td>
+                <td style={{color: `${change > 0 ? 'green' : 'red'}`}}>
+                    <div className="d-flex align-items-center">
+
+                    {change > 0 ? <FaCaretUp /> : <FaCaretDown />}
+                    {change}
+                    </div>
+                </td>
+                <td style={{color: `${color}`}}>
+                    <a href={coinrankingUrl} target="_blank" rel="noreferrer">
+                        <FaChartLine style={{fontSize: "2rem", color: `#3CC8C8`}} />
+                    </a>
+                </td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,31 @@ import Table from '../components/Table'
 import axios from 'axios'
 
 const Home = () => {
+  const [coins, setCoins] = useState([])
+  const getCoins = async () => {
+    const url = `https://api.coinranking.com/v2/coins/`;
 
+    const options = {
+      headers: { "x-access-token": process.env.REACT_APP_API_KEY },
+    };
+
+    try {
+      const {data} = await axios(url, options)
+      setCoins(data.data.coins)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getCoins()
+  }, [])
   
   return (
     <>
+        <h1 className='text-center mt-2 fw-bold'>Cyripto Coin App</h1>
         <Search />
-        <Table />
+        <Table coins={coins} />
     </>
   )
 }
